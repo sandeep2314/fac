5096875c314af86000198a7646423fd863359c2e
sandeep2314 <collegemanager@gmail.com>
2020-04-24 15:05:59 +0530

    mah

--- app/src/main/java/com/example/face/MainActivity.java
+++ app/src/main/java/com/example/face/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.face;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import static java.sql.Types.NULL;
 
@@ -12,7 +14,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+
+
+        findViewById(R.id.buttonRegister).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+                startActivity(new Intent(getApplication(),LoginActivity.class));
+            }
+        });
     }
+
+
+
 
 
 }
---
+++ app/src/main/java/va.java
@@ -0,0 +1,34 @@
+public class va {
+
+
+
+
+
+            private int id;
+        private String username, email, gender;
+
+        va(int id, String username, String email, String gender) {
+            this.id = id;
+            this.username = username;
+            this.email = email;
+            this.gender = gender;
+        }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+
+
+
+
+
+        }
