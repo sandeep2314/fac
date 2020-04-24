ba6cefa0957b207424a67ea51ffe7d245e06d808
sandeep2314 <collegemanager@gmail.com>
2020-04-24 13:32:02 +0530

    mah

--- app/src/main/java/com/example/face/MainActivity.java
+++ app/src/main/java/com/example/face/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import static java.sql.Types.NULL;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -11,4 +13,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
 }
