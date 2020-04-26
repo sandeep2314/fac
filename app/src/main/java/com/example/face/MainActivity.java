f355622cb0d5f9b64a63f5b67e71ea09e4d3a398
sandeep2314 <collegemanager@gmail.com>
2020-04-26 19:22:15 +0530

    mah

--- app/src/main/java/com/example/face/MainActivity.java
+++ app/src/main/java/com/example/face/MainActivity.java
@@ -6,6 +6,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import static java.sql.Types.NULL;
 
@@ -14,26 +17,29 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+      setContentView(R.layout.activity_product);
 
 
 
 
-
-        findViewById(R.id.buttonRegister).setOnClickListener(new View.OnClickListener() {
-
-
+        findViewById(R.id.buttontextViewRegister_product).setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
+
                 finish();
-               startActivity(new Intent(getApplication(),LoginActivity.class
-                ));
-            }
+                startActivity(new Intent(getApplicationContext(), ProfileActivity.class));
+
+
+
+            ;}
+
         });
+
+
     }
 
 
 
 
 
-}
+    }
---
+++ app/src/main/java/com/example/face/ProfileActivity.java
@@ -0,0 +1,46 @@
+package com.example.face;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+    public class ProfileActivity extends AppCompatActivity {
+
+
+        TextView textViewId, textViewUsername, textViewEmail, textViewGender;
+
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_profile);
+
+
+            textViewId = (TextView) findViewById(R.id.textViewId);
+            textViewUsername = (TextView) findViewById(R.id.textViewUsername);
+            textViewEmail = (TextView) findViewById(R.id.textViewEmail);
+            textViewGender = (TextView) findViewById(R.id.textViewGender);
+
+            findViewById(R.id.buttonLogout).setOnClickListener(new View.OnClickListener() {
+                                                                   @Override
+                                                                   public void onClick(View view) {
+                                                                       finish();
+                                                                       2314
+
+                                                                   }
+
+                                                               }
+
+
+
+            );
+
+        }
+
+    }
+
+
