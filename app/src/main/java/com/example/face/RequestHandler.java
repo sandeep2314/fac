17ad6d94fcd1ab0034238620dad0470cadf587ec
sandeep2314 <collegemanager@gmail.com>
2020-04-26 15:31:24 +0530

    mah

---
+++ app/src/main/java/com/example/face/RequestHandler.java
@@ -0,0 +1,6 @@
+package com.example.face;
+
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.HashMap;
--- app/src/main/java/com/example/face/URLs.java
+++ app/src/main/java/com/example/face/URLs.java
@@ -1,4 +1,18 @@
 package com.example.face;
 
 public class URLs {
-}
+
+
+
+
+        private static final String ROOT_URL = "http://192.168.101.1/Android/Api.php?apicall=";
+
+        public static final String URL_REGISTER = ROOT_URL + "signup";
+        public static final String URL_LOGIN= ROOT_URL + "login";
+
+
+
+
+    }
+
+
