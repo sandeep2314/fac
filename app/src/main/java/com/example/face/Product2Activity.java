a152ff8b42841a53a4444c4879b3d740a289a6a3
sandeep2314 <collegemanager@gmail.com>
2020-05-09 18:55:16 +0530

    Email

--- app/src/main/java/com/example/face/Product2Activity.java
+++ app/src/main/java/com/example/face/Product2Activity.java
@@ -10,6 +10,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import java.util.ArrayList;
 import java.util.List;
 
+
 public class Product2Activity extends AppCompatActivity {
     List<Product> productList;
 
